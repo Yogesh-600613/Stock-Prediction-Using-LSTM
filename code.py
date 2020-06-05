45a6bac11bd4ed5e73fc3d902da7e26d58d090d7
Yogesh Manwani <iit2017072@iiita.ac.in>
2020-06-05 13:12:16 +0530

    code update

--- code.py
+++ code.py
@@ -3,4 +3,12 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import datetime as dt
 
-dataset = pd.read_csv()
+dataset = pd.read_csv(r'Google_Stock_Price_Train.csv', index_col="Date", parse_dates=True)
+
+# print(dataset.head())
+# print(dataset.isna().any())
+# print(dataset.info())
+
+plt.subplot(2, 1, 1)
+dataset['Open'].plot(figsize=(16, 6))
+plt.show()
